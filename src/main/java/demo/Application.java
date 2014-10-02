68bafa85003374db352b2af04bca08dbdcba593d
William Edwards <will.edwards@me.com>
2014-10-02 13:50:34 +0100

    tidy up

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,17 +1,9 @@
 package demo;
 
-import demo.service.SimpleItemQueue;
-import demo.servlets.SelfPurgingQueueServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.WebApplicationInitializer;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 @Configuration
 @EnableAutoConfiguration
@@ -19,13 +11,6 @@ import javax.servlet.ServletRegistration;
 public class Application
 {
 
-    private static final SimpleItemQueue simpleItemQueue = new SimpleItemQueue();
-
-    @Bean
-    public SimpleItemQueue buildQueue()
-    {
-        return simpleItemQueue;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
